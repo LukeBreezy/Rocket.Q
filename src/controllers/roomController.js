62b0949f649a32540ebfb503562b49a4c2d53e08
Lucas de Paula Oliveira Santos <lkslukas23@gmail.com>
2021-08-02 00:42:48 -0300

    Aula 4 - Concluída

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -1,7 +1,25 @@
-module.exports = {
-    create(req, res){
+const Database = require('../db/config')
 
-        let roomId = '12346'
+module.exports = {
+
+    async create(req, res){
+
+        
+        const password = req.body.password
+        let roomId = ''
+        
+        for(let i = 0; i < 6; i++){
+            roomId += Math.floor(Math.random() * 10)
+        }
+        
+        const db = await Database()
+
+        await db.run(`
+            INSERT INTO room VALUES
+            ('${roomId}', '${password}')
+        `)
+
+        await db.close()
 
         res.redirect(`/room/${roomId}`)
     }
